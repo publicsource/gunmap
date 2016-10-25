dc97aa6a9b0a8f367c6751c3bcbb0d71bd45a930
Alexandra Kanik <alexandra@rationalact.com>
2016-10-25 15:37:12 -0400

    fix issues with invalid lat/long error message. likely data had column with no header.

--- geojson/test.js
+++ geojson/test.js
@@ -1,257 +1,251 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1345\cocoasubrtf380
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
-
-\f0\fs24 \cf0 var pgh_murders = \{\
-    "type": "FeatureCollection", \
-    "bbox": [\
-        -80.01475525, \
-        40.44017226, \
-        -79.89613406, \
-        40.46665573\
-    ], \
-    "features": [\
-        \{\
-            "type": "Feature", \
-            "geometry": \{\
-                "type": "Point", \
-                "coordinates": [\
-                    -80.007591, \
-                    40.462894\
-                ]\
-            \}, \
-            "properties": \{\
-                "VICTIM_ID": "3", \
-                "TYPE": "Homicide", \
-                "CCR": "", \
-                "DATE": "1/20/10", \
-                "TIME": "235", \
-                "HOUR": "2", \
-                "LOCATION": "1923 MOUNTFORD AV", \
-                "bing_type": "Address", \
-                "bing_confidence": "High", \
-                "bing_latitude": "40.462894", \
-                "hand_latitude": "", \
-                "bing_longitude": "-80.007591", \
-                "hand_longitude": "", \
-                "mapquest_type": "house", \
-                "mapquest_class": "place", \
-                "mapquest_latitude": "40.46297943", \
-                "mapquest_longitude": "-80.00736846", \
-                "CENSUS_TRACT": "2509", \
-                "NEIGHBORHOOD": "Fineview", \
-                "ZONE": "1", \
-                "WEAPON": "HANDGUN", \
-                "": "Firearm", \
-                "MOTIVE": "", \
-                "VICTIM_RACE": "BLACK", \
-                "VICTIM_SEX": "MALE", \
-                "VICTIM_AGE": "50", \
-                "ACTOR_CT.": "0"\
-            \}\
-        \}, \
-        \{\
-            "type": "Feature", \
-            "geometry": \{\
-                "type": "Point", \
-                "coordinates": [\
-                    -79.89613406, \
-                    40.45730822\
-                ]\
-            \}, \
-            "properties": \{\
-                "VICTIM_ID": "4", \
-                "TYPE": "Homicide", \
-                "CCR": "", \
-                "DATE": "2/13/10", \
-                "TIME": "2215", \
-                "HOUR": "22", \
-                "LOCATION": "7200 FRANKSTOWN AV", \
-                "bing_type": "Address", \
-                "bing_confidence": "High", \
-                "bing_latitude": "40.45730822", \
-                "hand_latitude": "", \
-                "bing_longitude": "-79.89613406", \
-                "hand_longitude": "", \
-                "mapquest_type": "house", \
-                "mapquest_class": "place", \
-                "mapquest_latitude": "40.457283", \
-                "mapquest_longitude": "-79.896174", \
-                "CENSUS_TRACT": "1303", \
-                "NEIGHBORHOOD": "Homewood South", \
-                "ZONE": "5", \
-                "WEAPON": "HANDGUN", \
-                "": "Firearm", \
-                "MOTIVE": "", \
-                "VICTIM_RACE": "BLACK", \
-                "VICTIM_SEX": "MALE", \
-                "VICTIM_AGE": "31", \
-                "ACTOR_CT.": "1"\
-            \}\
-        \}, \
-        \{\
-            "type": "Feature", \
-            "geometry": \{\
-                "type": "Point", \
-                "coordinates": [\
-                    -80.01475525, \
-                    40.46665573\
-                ]\
-            \}, \
-            "properties": \{\
-                "VICTIM_ID": "5", \
-                "TYPE": "Homicide", \
-                "CCR": "", \
-                "DATE": "2/14/10", \
-                "TIME": "16", \
-                "HOUR": "0", \
-                "LOCATION": "2321 HOLYOKE ST", \
-                "bing_type": "Address", \
-                "bing_confidence": "High", \
-                "bing_latitude": "40.46665573", \
-                "hand_latitude": "", \
-                "bing_longitude": "-80.01475525", \
-                "hand_longitude": "", \
-                "mapquest_type": "house", \
-                "mapquest_class": "place", \
-                "mapquest_latitude": "40.46666155", \
-                "mapquest_longitude": "-80.01466182", \
-                "CENSUS_TRACT": "2614", \
-                "NEIGHBORHOOD": "Perry South", \
-                "ZONE": "1", \
-                "WEAPON": "HANDGUN", \
-                "": "Firearm", \
-                "MOTIVE": "", \
-                "VICTIM_RACE": "BLACK", \
-                "VICTIM_SEX": "MALE", \
-                "VICTIM_AGE": "21", \
-                "ACTOR_CT.": "0"\
-            \}\
-        \}, \
-        \{\
-            "type": "Feature", \
-            "geometry": \{\
-                "type": "Point", \
-                "coordinates": [\
-                    -79.97675121, \
-                    40.44290945\
-                ]\
-            \}, \
-            "properties": \{\
-                "VICTIM_ID": "6", \
-                "TYPE": "Homicide", \
-                "CCR": "", \
-                "DATE": "2/22/10", \
-                "TIME": "1948", \
-                "HOUR": "19", \
-                "LOCATION": "2109 ELMORE SQ", \
-                "bing_type": "Address", \
-                "bing_confidence": "High", \
-                "bing_latitude": "40.44290945", \
-                "hand_latitude": "", \
-                "bing_longitude": "-79.97675121", \
-                "hand_longitude": "", \
-                "mapquest_type": "house", \
-                "mapquest_class": "place", \
-                "mapquest_latitude": "40.44288758", \
-                "mapquest_longitude": "-79.97698912", \
-                "CENSUS_TRACT": "511", \
-                "NEIGHBORHOOD": "Terrace Village", \
-                "ZONE": "2", \
-                "WEAPON": "HANDGUN", \
-                "": "Firearm", \
-                "MOTIVE": "", \
-                "VICTIM_RACE": "BLACK", \
-                "VICTIM_SEX": "MALE", \
-                "VICTIM_AGE": "22", \
-                "ACTOR_CT.": "0"\
-            \}\
-        \}, \
-        \{\
-            "type": "Feature", \
-            "geometry": \{\
-                "type": "Point", \
-                "coordinates": [\
-                    -79.975675, \
-                    40.451591\
-                ]\
-            \}, \
-            "properties": \{\
-                "VICTIM_ID": "7", \
-                "TYPE": "Homicide", \
-                "CCR": "", \
-                "DATE": "2/25/10", \
-                "TIME": "1506", \
-                "HOUR": "15", \
-                "LOCATION": "PAULOWNA ST & BIGELOW BL", \
-                "bing_type": "", \
-                "bing_confidence": "", \
-                "bing_latitude": "", \
-                "hand_latitude": "40.451591", \
-                "bing_longitude": "", \
-                "hand_longitude": "-79.975675", \
-                "mapquest_type": "", \
-                "mapquest_class": "", \
-                "mapquest_latitude": "", \
-                "mapquest_longitude": "", \
-                "CENSUS_TRACT": "605", \
-                "NEIGHBORHOOD": "Polish Hill", \
-                "ZONE": "2", \
-                "WEAPON": "HANDGUN", \
-                "": "Firearm", \
-                "MOTIVE": "", \
-                "VICTIM_RACE": "BLACK", \
-                "VICTIM_SEX": "MALE", \
-                "VICTIM_AGE": "40", \
-                "ACTOR_CT.": "0"\
-            \}\
-        \}, \
-        \{\
-            "type": "Feature", \
-            "geometry": \{\
-                "type": "Point", \
-                "coordinates": [\
-                    -79.97715823, \
-                    40.44017226\
-                ]\
-            \}, \
-            "properties": \{\
-                "VICTIM_ID": "8", \
-                "TYPE": "Homicide", \
-                "CCR": "", \
-                "DATE": "3/3/10", \
-                "TIME": "1354", \
-                "HOUR": "13", \
-                "LOCATION": "2100 bl BENTLEY DR", \
-                "bing_type": "Address", \
-                "bing_confidence": "High", \
-                "bing_latitude": "40.44017226", \
-                "hand_latitude": "", \
-                "bing_longitude": "-79.97715823", \
-                "hand_longitude": "", \
-                "mapquest_type": "", \
-                "mapquest_class": "", \
-                "mapquest_latitude": "", \
-                "mapquest_longitude": "", \
-                "CENSUS_TRACT": "511", \
-                "NEIGHBORHOOD": "Terrace Village", \
-                "ZONE": "2", \
-                "WEAPON": "HANDGUN", \
-                "": "Firearm", \
-                "MOTIVE": "", \
-                "VICTIM_RACE": "BLACK", \
-                "VICTIM_SEX": "MALE", \
-                "VICTIM_AGE": "56", \
-                "ACTOR_CT.": "0"\
-            \}\
-        \}\
-    ], \
-    "crs": \{\
-        "type": "name", \
-        "properties": \{\
-            "name": "EPSG:4269"\
-        \}\
-    \}\
-\}}
+var pgh_murders = {
+    "type": "FeatureCollection", 
+    "bbox": [
+        -80.01475525, 
+        40.44017226, 
+        -79.89613406, 
+        40.46665573
+    ], 
+    "features": [
+        {
+            "type": "Feature", 
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -80.007591, 
+                    40.462894
+                ]
+            }, 
+            "properties": {
+                "VICTIM_ID": "3", 
+                "TYPE": "Homicide", 
+                "CCR": "", 
+                "DATE": "1/20/10", 
+                "TIME": "235", 
+                "HOUR": "2", 
+                "LOCATION": "1923 MOUNTFORD AV", 
+                "bing_type": "Address", 
+                "bing_confidence": "High", 
+                "bing_latitude": "40.462894", 
+                "hand_latitude": "", 
+                "bing_longitude": "-80.007591", 
+                "hand_longitude": "", 
+                "mapquest_type": "house", 
+                "mapquest_class": "place", 
+                "mapquest_latitude": "40.46297943", 
+                "mapquest_longitude": "-80.00736846", 
+                "CENSUS_TRACT": "2509", 
+                "NEIGHBORHOOD": "Fineview", 
+                "ZONE": "1", 
+                "WEAPON": "HANDGUN", 
+                "": "Firearm", 
+                "MOTIVE": "", 
+                "VICTIM_RACE": "BLACK", 
+                "VICTIM_SEX": "MALE", 
+                "VICTIM_AGE": "50", 
+                "ACTOR_CT.": "0"
+            }
+        }, 
+        {
+            "type": "Feature", 
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -79.89613406, 
+                    40.45730822
+                ]
+            }, 
+            "properties": {
+                "VICTIM_ID": "4", 
+                "TYPE": "Homicide", 
+                "CCR": "", 
+                "DATE": "2/13/10", 
+                "TIME": "2215", 
+                "HOUR": "22", 
+                "LOCATION": "7200 FRANKSTOWN AV", 
+                "bing_type": "Address", 
+                "bing_confidence": "High", 
+                "bing_latitude": "40.45730822", 
+                "hand_latitude": "", 
+                "bing_longitude": "-79.89613406", 
+                "hand_longitude": "", 
+                "mapquest_type": "house", 
+                "mapquest_class": "place", 
+                "mapquest_latitude": "40.457283", 
+                "mapquest_longitude": "-79.896174", 
+                "CENSUS_TRACT": "1303", 
+                "NEIGHBORHOOD": "Homewood South", 
+                "ZONE": "5", 
+                "WEAPON": "HANDGUN", 
+                "": "Firearm", 
+                "MOTIVE": "", 
+                "VICTIM_RACE": "BLACK", 
+                "VICTIM_SEX": "MALE", 
+                "VICTIM_AGE": "31", 
+                "ACTOR_CT.": "1"
+            }
+        }, 
+        {
+            "type": "Feature", 
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -80.01475525, 
+                    40.46665573
+                ]
+            }, 
+            "properties": {
+                "VICTIM_ID": "5", 
+                "TYPE": "Homicide", 
+                "CCR": "", 
+                "DATE": "2/14/10", 
+                "TIME": "16", 
+                "HOUR": "0", 
+                "LOCATION": "2321 HOLYOKE ST", 
+                "bing_type": "Address", 
+                "bing_confidence": "High", 
+                "bing_latitude": "40.46665573", 
+                "hand_latitude": "", 
+                "bing_longitude": "-80.01475525", 
+                "hand_longitude": "", 
+                "mapquest_type": "house", 
+                "mapquest_class": "place", 
+                "mapquest_latitude": "40.46666155", 
+                "mapquest_longitude": "-80.01466182", 
+                "CENSUS_TRACT": "2614", 
+                "NEIGHBORHOOD": "Perry South", 
+                "ZONE": "1", 
+                "WEAPON": "HANDGUN", 
+                "": "Firearm", 
+                "MOTIVE": "", 
+                "VICTIM_RACE": "BLACK", 
+                "VICTIM_SEX": "MALE", 
+                "VICTIM_AGE": "21", 
+                "ACTOR_CT.": "0"
+            }
+        }, 
+        {
+            "type": "Feature", 
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -79.97675121, 
+                    40.44290945
+                ]
+            }, 
+            "properties": {
+                "VICTIM_ID": "6", 
+                "TYPE": "Homicide", 
+                "CCR": "", 
+                "DATE": "2/22/10", 
+                "TIME": "1948", 
+                "HOUR": "19", 
+                "LOCATION": "2109 ELMORE SQ", 
+                "bing_type": "Address", 
+                "bing_confidence": "High", 
+                "bing_latitude": "40.44290945", 
+                "hand_latitude": "", 
+                "bing_longitude": "-79.97675121", 
+                "hand_longitude": "", 
+                "mapquest_type": "house", 
+                "mapquest_class": "place", 
+                "mapquest_latitude": "40.44288758", 
+                "mapquest_longitude": "-79.97698912", 
+                "CENSUS_TRACT": "511", 
+                "NEIGHBORHOOD": "Terrace Village", 
+                "ZONE": "2", 
+                "WEAPON": "HANDGUN", 
+                "": "Firearm", 
+                "MOTIVE": "", 
+                "VICTIM_RACE": "BLACK", 
+                "VICTIM_SEX": "MALE", 
+                "VICTIM_AGE": "22", 
+                "ACTOR_CT.": "0"
+            }
+        }, 
+        {
+            "type": "Feature", 
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -79.975675, 
+                    40.451591
+                ]
+            }, 
+            "properties": {
+                "VICTIM_ID": "7", 
+                "TYPE": "Non-fatal Shooting", 
+                "CCR": "", 
+                "DATE": "2/25/10", 
+                "TIME": "1506", 
+                "HOUR": "15", 
+                "LOCATION": "PAULOWNA ST & BIGELOW BL", 
+                "bing_type": "", 
+                "bing_confidence": "", 
+                "bing_latitude": "", 
+                "hand_latitude": "40.451591", 
+                "bing_longitude": "", 
+                "hand_longitude": "-79.975675", 
+                "mapquest_type": "", 
+                "mapquest_class": "", 
+                "mapquest_latitude": "", 
+                "mapquest_longitude": "", 
+                "CENSUS_TRACT": "605", 
+                "NEIGHBORHOOD": "Polish Hill", 
+                "ZONE": "2", 
+                "WEAPON": "HANDGUN", 
+                "": "Firearm", 
+                "MOTIVE": "", 
+                "VICTIM_RACE": "BLACK", 
+                "VICTIM_SEX": "MALE", 
+                "VICTIM_AGE": "40", 
+                "ACTOR_CT.": "0"
+            }
+        }, 
+        {
+            "type": "Feature", 
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -79.97715823, 
+                    40.44017226
+                ]
+            }, 
+            "properties": {
+                "VICTIM_ID": "8", 
+                "TYPE": "pie", 
+                "CCR": "", 
+                "DATE": "3/3/10", 
+                "TIME": "1354", 
+                "HOUR": "13", 
+                "LOCATION": "2100 bl BENTLEY DR", 
+                "bing_type": "Address", 
+                "bing_confidence": "High", 
+                "bing_latitude": "40.44017226", 
+                "hand_latitude": "", 
+                "bing_longitude": "-79.97715823", 
+                "hand_longitude": "", 
+                "mapquest_type": "", 
+                "mapquest_class": "", 
+                "mapquest_latitude": "", 
+                "mapquest_longitude": "", 
+                "CENSUS_TRACT": "511", 
+                "NEIGHBORHOOD": "Terrace Village", 
+                "ZONE": "2", 
+                "WEAPON": "HANDGUN", 
+                "": "Firearm", 
+                "MOTIVE": "", 
+                "VICTIM_RACE": "BLACK", 
+                "VICTIM_SEX": "MALE", 
+                "VICTIM_AGE": "56", 
+                "ACTOR_CT.": "0"
+            }
+        }
+    ], 
+    "crs": {
+        "type": "name", 
+        "properties": {
+            "name": "EPSG:4269"
+        }
+    }
+}
